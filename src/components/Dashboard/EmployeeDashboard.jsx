05bcbe806ee19751e7ad4951e72da0c3083783d8
SyedAaquibQuadri <syedaaquibquadri@gmail.com>
2026-06-05 19:01:07 +0530

    Add useContext import to EmployeeDashboard component

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import Header from '../other/Header'
 import TaskListNumbers from '../other/TaskListNumbers'
 import TaskList from '../TaskList/TaskList'
